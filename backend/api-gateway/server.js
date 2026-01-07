4e21f4e41c6c5408cf85fc9a6b75ff983760728c
Rohan-Panchal-Wintree <rohanpanchal@wintreetech.com>
2026-01-07 19:15:05 +0530

    mend

--- backend/api-gateway/server.js
+++ backend/api-gateway/server.js
@@ -2,6 +2,10 @@ import express from "express";
 import expressProxy from "express-http-proxy";
 import { keys } from "./utils/keys.js";
 
+const env = process.env.ENV;
+
+const prodUrl = process.env.CLIENT_URL_PROD;
+
 const app = express();
 
 app.use("/crm", expressProxy("http://localhost:3901"));
